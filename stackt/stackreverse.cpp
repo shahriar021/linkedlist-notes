ea4ee87289ba39a0aa9a566e8ebb5658c88a9555
shahriar021 <shahriarc19@gmail.com>
2024-11-04 07:44:31 +0600

    stack

---
+++ stackt/stackreverse.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseStack(string s){
+    stack<string>st;
+
+
+    
+
+    for(int i=0;i<s.length();i++){
+       string word="";
+       while(s[i]!=' ' && i<s.length()){
+            word+=s[i];
+            i++;
+       }
+       st.push(word);
+    }
+
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }cout<<endl;
+   
+
+}
+
+
+int main(){
+
+    string st;
+
+    st="i love you";
+    reverseStack(st);
+
+
+    return 0;
+}
